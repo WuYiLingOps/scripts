5340f94e0c00eb23950a500419a6cfbee2eff7fb
無以菱 <2794998160@qq.com>
2025-12-13 16:17:16 +0800

    docs: 优化 install_php_yum.bash 注释头部格式

--- InstallSoftScript/install_php_yum.bash
+++ InstallSoftScript/install_php_yum.bash
@@ -1,7 +1,15 @@
 #!/bin/bash
-# author YiLing Wu (hj)
-# date 2023-12-23 15:21
-# description: centos下yum安装php.8.2版本
+#
+#********************************************************************
+#Author:           YiLing Wu (hj)
+#email:            huangjing510@126.com
+#Date:             2023-12-23
+#FileName:         install_php_yum.bash
+#URL:              http://42.194.242.109:510/
+#Description:      CentOS下使用YUM安装PHP 8.2版本
+#Copyright (C):    2024 All rights reserved
+#********************************************************************
+#
 
 yum install epel-release -y
 yum -y install https://mirrors.aliyun.com/remi/enterprise/remi-release-7.rpm
