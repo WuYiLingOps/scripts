eee721d0cb9352f04de3840f37d1f5ff78417ebf
無以菱 <2794998160@qq.com>
2025-12-13 15:55:28 +0800

    docs: 为 zk.bash 添加标准化注释头部

--- BigDataScript/zk.bash
+++ BigDataScript/zk.bash
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+#********************************************************************
+#Author:           YiLing Wu (hj)
+#email:            huangjing510@126.com
+#Date:             2024-12-23
+#FileName:         zk.bash
+#URL:              http://42.194.242.109:510/
+#Description:      Zookeeper集群管理脚本，支持启动、停止、查看状态
+#Copyright (C):    2024 All rights reserved
+#********************************************************************
+#
+# Usage: bash zk.bash [start|stop|status]
 
 # 获取所有主机名
 hostname=$(awk '!/^#/ && NF==2 {print $2}' /etc/hosts)
