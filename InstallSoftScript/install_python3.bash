8278b08be67ff560fbb569235b185307eea2640f
無以菱 <2794998160@qq.com>
2025-12-13 16:17:34 +0800

    docs: 优化 install_python3.bash 注释头部格式

--- InstallSoftScript/install_python3.bash
+++ InstallSoftScript/install_python3.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
-# author YiLing Wu (hj)
-# date 2023-12-23 15:21
-# description: centos下使用yum简易安装python3
+#
+#********************************************************************
+#Author:           YiLing Wu (hj)
+#email:            huangjing510@126.com
+#Date:             2023-12-23
+#FileName:         install_python3.bash
+#URL:              http://42.194.242.109:510/
+#Description:      CentOS下使用YUM简易安装Python3
+#Copyright (C):    2024 All rights reserved
+#********************************************************************
+#
 yum install epel-release -y
 yum install python3 -y
