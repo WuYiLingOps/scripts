f6740abe61240b7fbdaaea8db19872a60976e957
無以菱 <2794998160@qq.com>
2025-12-13 16:31:05 +0800

    docs: 优化 docker-自动补全.bash 注释头部格式并修正 shebang

--- docker-自动补全.bash
+++ docker-自动补全.bash
@@ -1,7 +1,15 @@
-#!/usr/bash
-# author YiLing Wu (hj)
-# date 2023-12-23 15:21
-# description: 配置docker自动补全
+#!/bin/bash
+#
+#********************************************************************
+#Author:           YiLing Wu (hj)
+#email:            huangjing510@126.com
+#Date:             2023-12-23
+#FileName:         docker-自动补全.bash
+#URL:              http://42.194.242.109:510/
+#Description:      配置Docker自动补全
+#Copyright (C):    2024 All rights reserved
+#********************************************************************
+#
 
 yum install -y bash-completion
 source /usr/share/bash-completion/bash_completion
